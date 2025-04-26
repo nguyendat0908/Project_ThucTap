ae61acab921c7c981e6bcfae02235fe4a391df50
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-04-27 01:06:16 +0700

    First commit

---
+++ src/main/java/com/example/Project_Jobhunter/ProjectJobhunterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_Jobhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJobhunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJobhunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project_Jobhunter/ProjectJobhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_Jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJobhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
