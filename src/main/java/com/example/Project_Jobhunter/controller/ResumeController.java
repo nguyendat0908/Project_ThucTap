2ce46add62c32f12e006257891fc6bf7125ae814
Nguyendat0908 <Nguyendat090803@gmail.com>
2025-05-30 00:17:42 +0700

    Hiển thị CV theo một công ty của người dùng đang đăng nhập

--- src/main/java/com/example/Project_Jobhunter/controller/ResumeController.java
+++ src/main/java/com/example/Project_Jobhunter/controller/ResumeController.java
@@ -3,6 +3,7 @@ package com.example.Project_Jobhunter.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Project_Jobhunter.domain.Company;
 import com.example.Project_Jobhunter.domain.Job;
 import com.example.Project_Jobhunter.domain.Resume;
 import com.example.Project_Jobhunter.domain.User;
@@ -11,9 +12,15 @@ import com.example.Project_Jobhunter.dto.response.ResResumeDTO;
 import com.example.Project_Jobhunter.service.JobService;
 import com.example.Project_Jobhunter.service.ResumeService;
 import com.example.Project_Jobhunter.service.UserService;
+import com.example.Project_Jobhunter.util.SecurityUtil;
 import com.example.Project_Jobhunter.util.annotation.ApiMessage;
 import com.example.Project_Jobhunter.util.exception.IdInvalidException;
 import com.turkraft.springfilter.boot.Filter;
+import com.turkraft.springfilter.builder.FilterBuilder;
+import com.turkraft.springfilter.converter.FilterSpecificationConverter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -33,11 +40,16 @@ public class ResumeController {
     private final ResumeService resumeService;
     private final UserService userService;
     private final JobService jobService;
+    private final FilterSpecificationConverter filterSpecificationConverter;
+    private final FilterBuilder filterBuilder;
 
-    public ResumeController(ResumeService resumeService, UserService userService, JobService jobService) {
+    public ResumeController(ResumeService resumeService, UserService userService, JobService jobService,
+            FilterSpecificationConverter filterSpecificationConverter, FilterBuilder filterBuilder) {
         this.resumeService = resumeService;
         this.userService = userService;
         this.jobService = jobService;
+        this.filterSpecificationConverter = filterSpecificationConverter;
+        this.filterBuilder = filterBuilder;
     }
 
     @PostMapping("/resumes")
@@ -105,6 +117,7 @@ public class ResumeController {
     }
 
     @GetMapping("/resumes-history/{id}")
+    @ApiMessage("Hiển thị lịch sử các CV đã nộp thành công!")
     public ResponseEntity<ResPaginationDTO> getResumesHistoryByUser(@PathVariable("id") int id, Pageable pageable)
             throws IdInvalidException {
         User user = this.userService.handleGetUserById(id);
@@ -114,6 +127,31 @@ public class ResumeController {
         }
 
         return ResponseEntity.ok(this.resumeService.handleGetResumesHistory(id, pageable));
+    }
+
+    @GetMapping("/resumes-company")
+    @ApiMessage("Hiển thị các CV của công ty thành công!")
+    public ResponseEntity<ResPaginationDTO> getResumesOfCompany(@Filter Specification<Resume> spec, Pageable pageable) {
+        List<Integer> arrJobIds = null;
+        String email = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        User currentUser = this.userService.handleGetUserByUsername(email);
+        if (currentUser != null) {
+            Company company = currentUser.getCompany();
+            if (company != null) {
+                List<Job> companyJobs = company.getJobs();
+                if (companyJobs != null && companyJobs.size() > 0) {
+                    arrJobIds = companyJobs.stream().map(item -> item.getId()).collect(Collectors.toList());
+                }
+            }
+        }
+
+        Specification<Resume> jobInSpec = filterSpecificationConverter
+                .convert(filterBuilder.field("job").in(filterBuilder.input(arrJobIds)).get());
+
+        Specification<Resume> finalSpec = jobInSpec.and(spec);
+
+        return ResponseEntity.ok(this.resumeService.handleGetAllResumes(finalSpec, pageable));
     }
 
 }
